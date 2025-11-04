0cec689aa4b872f0c279938e4af8abd4d2a26e03
yxzhan <yanxiang@uni-bremen.de>
2025-11-04 14:50:12 +0100

    test kernel

---
+++ binder/isaacsim_python_wrapper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Isaac-sim python exe wrapper
+
+# unset virtualGL ENV (which breaks ROS2 bridge)
+unset LD_PRELOAD
+# Clear default ROS ENV
+unset PYTHONPATH
+export LD_LIBRARY_PATH=$ISAACSIM_PATH/exts/isaacsim.ros2.bridge/$ROS_DISTRO/lib
+
+$ISAACSIM_PYTHON_EXE "$@"
