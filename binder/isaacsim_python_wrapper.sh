e7d91eef48a34403d0777e19bd8ed2ac8e11b480
yxzhan <yanxiang@uni-bremen.de>
2025-11-06 12:25:59 +0100

    do not use built ros

--- binder/isaacsim_python_wrapper.sh
+++ binder/isaacsim_python_wrapper.sh
@@ -11,7 +11,7 @@ unset PYTHONPATH
 export LD_LIBRARY_PATH=$ISAACSIM_PATH/exts/isaacsim.ros2.bridge/$ROS_DISTRO/lib
 
 # # setup python 3.11 ROS jazzy environment
-source /mnt/dev-tools/ros_ws/jazzy_py311/install/setup.bash
+# source /mnt/dev-tools/ros_ws/jazzy_py311/install/setup.bash
 
 # # setup Isaac Sim examples ROS workspace
 # source /mnt/dev-tools/ros_ws/isaacsim_ws/install/setup.bash
